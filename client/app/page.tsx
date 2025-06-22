30fb3fcd809ac5dfd05353ae6d4045586ad71edd
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-06-23 01:01:04 +0900

    Add Contact list ui

--- client/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const HomePage = () => {
-	return <div className='font-spaceGrotesk'>HomePage</div>
-}
-
-export default HomePage
--- client/components/providers/theme.provider.tsx
+++ client/components/providers/theme.provider.tsx
@@ -2,15 +2,10 @@
 
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import NoSSR from 'react-no-ssr'
 
 export function ThemeProvider({
 	children,
 	...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
-	return (
-		<NextThemesProvider {...props}>
-			<NoSSR>{children}</NoSSR>
-		</NextThemesProvider>
-	)
+	return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
