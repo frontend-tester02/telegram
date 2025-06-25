3f1b004c4fa91855ef68f6200aba4b3e84daec04
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-06-26 02:05:11 +0900

    Express application init

--- server/app.js
+++ server/app.js
@@ -1,1 +1,16 @@
-console.log('Hello')
+require('dotenv').config()
+
+const express = require('express')
+const http = require('http')
+const cookieParser = require('cookie-parser')
+
+const app = express()
+
+// Middleware
+app.use(express.json())
+
+app.use('/api', require('./routes/index'))
+
+const PORT = process.env.PORT || 6000
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
