4b30dc40f665469230785e4952e1db1b4701250d
Shokhrukh <shokhrukhnuriddinov@gmail.com>
2025-07-01 01:20:33 +0900

    Add user api route

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,7 +2,20 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/user.controller')
 
+router.get('/contacts', userController.getContacts)
 router.get('/messages/:contactId', userController.getMessages)
-router.post('/create-message', userController.createMessage)
+
+router.post('/message', userController.createMessage)
+router.post('/message-read', userController.messageRead)
+router.post('/contact', userController.createContact)
+router.post('/reaction', userController.createReaction)
+router.post('/send-otp', userController.sendOtp)
+
+router.put('/profile', userController.updateProfile)
+router.put('/message/:messageId', userController.updateMessage)
+router.put('/email', userController.updateEmail)
+
+router.delete('/', userController.deleteUser)
+router.delete('/message/:messageId', userController.deleteMessage)
 
 module.exports = router
